25d029794521385217c0645961a10835dd4e8ca8
Gerardo <gerardo.manzano21@gmail.com>
2022-03-25 09:23:55 -0600

    First Commit

---
+++ guessmynumber/src/components/game/game.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"; 
+import '.Game.css'
+class Game extends Component {
+    constructor(){
+    super()    
+
+class Game extends Component {
+    render(){ 
+        return(
+    <div className="game">
+        
+        <imput
+         type ="number"
+         />
+          <button>probar</button>
+          <p>Texto</p>
+         </div>
+        );
+}   }
+
